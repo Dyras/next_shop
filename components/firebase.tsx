c109b28c2e0644c92beba22ef02c0c2d1b60e2ba
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-03-30 13:27:16 +0200

    Add firebase!

---
+++ components/firebase.tsx
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBC1NJAt-cD7SoXhFKL3YzLMrxuI0C-lBA',
+    authDomain: 'angularshop-3d1ea.firebaseapp.com',
+    databaseURL: 'https://angularshop-3d1ea.firebaseio.com',
+    projectId: 'angularshop-3d1ea',
+    storageBucket: 'angularshop-3d1ea.appspot.com',
+    messagingSenderId: '701167913609',
+    appId: '1:701167913609:web:240ec3668f65c2ac559458',
+  };
+
+// Initialize a Firebase app
+const app = initializeApp(firebaseConfig)
+
+// Get a Firestore instance
+const db = getFirestore(app)
+
+export { db }
--- pages/products.tsx
+++ pages/products.tsx
@@ -1,7 +1,62 @@
+import { db } from "@/components/firebase"
+import Navbar from "@/components/navbar/navbar";
+import { collection, getDocs } from "firebase/firestore"
 
 export default function Products() {
+getFirestoreDocs()
     return(
+<>
+<Navbar />
 <div>Test</div>
+</>
 )
 }
+export interface IProduct {
+    id: string;
+    name: string;
+    manufacturer?: string;
+    description?: string;
+    articleType: string;
+    country?: string;
+    price: number;
+    rating?: number;
+    imageUrl?: string;
+    outOfStock: boolean;
+    slug: string;
+    publishedAt?: Date;
+    packaging: string;
+    vintage?: number;
+  }
+async function getFirestoreDocs() {
+    const Products: IProduct[] = []
+    const docRef = await getDocs(collection(db, "products"))
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            Products.push({
+                id: doc.id,
+                name: doc.data().name,
+                manufacturer: doc.data().manufacturer,
+                description: doc.data().description,
+                articleType: doc.data().articleType,
+                country: doc.data().country,
+                price: doc.data().price,
+                rating: doc.data().rating,
+                imageUrl: doc.data().imageUrl,
+                outOfStock: doc.data().outOfStock,
+                slug: doc.data().slug,
+                publishedAt: doc.data().publishedAt,
+                packaging: doc.data().packaging,
+                vintage: doc.data().vintage
+            })
+        });
+    })
+.catch((error) => {
+    console.log("Error getting document:", error);
+})
+if (Products.length > 0){
+console.log(Products)
+}
+}
+
 
