b7652e6eb9a5ab89657e0facdc312dd752d09847
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-03-31 12:09:47 +0200

    Make [id] use a useEffect for the product

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,32 +2,37 @@ import { db } from "@/components/firebase";
 import { IProduct } from "@/lib/iproduct";
 import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export async function GetProduct(){
-    const router = useRouter();
-    const { id } = router.query;
-    if (typeof id !== "string") return null;
-    const docRef = doc(db, "products", id);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
+export default function SingleProduct() {
+  const router = useRouter();
+  const [product, setProduct] = useState<IProduct | null>(null);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const { id } = router.query;
+      if (typeof id !== "string") return;
+
+      const docRef = doc(db, "products", id);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
         console.log("Document data:", docSnap.data() as IProduct);
-        return docSnap.data() as IProduct;
-    }
-    else {
-        console.log("No such document!");
-        return null;
-    }
-}
+        setProduct(docSnap.data() as IProduct);
+      } else {
+        console.log("Document not found!");
+        setProduct(null);
+      }
+    };
 
-export default function SingleProduct(){
-    const product = GetProduct();
-    
-    if (!product) return (<div>Produkten hittades inte</div>);
-        return (
-            <div>
-        <div>Produkten hittades</div>
-        <div>{product.id}</div>
-        <div>{product.name}</div>
-        <div>{product.manufacturer}</div>
-        </div>
-)    }
+    getProduct();
+  }, [router.query]);
+
+  if (!product) return <div>Produkten hittades inte</div>;
+
+  return (
+    <div>
+      <div>Produkten hittades</div>
+      <div>{product.name}</div>
+    </div>
+  );
+}
