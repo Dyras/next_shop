f3fbcb790751f0ac17353e401d184423172d72c1
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-04-03 15:57:30 +0200

    Print out cart content on the cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,20 +1,33 @@
+import { cart } from "@/components/cartstorage";
 import Navbar from "@/components/navbar/navbar";
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
+import { useAtom } from "jotai";
 
+export default function Cart() {
+	const [cartValue] = useAtom(cart);
 
-export default function Cart(){
-return (
-    <>
-    <Navbar />
-    <div className={styles.container}>
-        <div className={styles.grid}>
-            <div className={styles.card}>
-                <h2>Kundvagn</h2>
-                <p>Det finns inga produkter i din kundvagn.</p>
-            </div>
-        </div>
-    </div>
-    </>
-)
-
+	return (
+		<>
+			<Navbar />
+			<div className={styles.container}>
+				<div className={styles.grid}>
+					<div className={styles.card}>
+						<h2>Kundkorg</h2>
+						<div>
+							{cartValue.length > 0 ? (
+								cartValue.map((item) => (
+									<div key={item.id}>
+										<p>{item.name}</p>
+										<p>{item.amount}</p>
+									</div>
+								))
+							) : (
+								<p>Kundkorgen är tom</p>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
