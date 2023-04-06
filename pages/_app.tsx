2ddffad134659ab5028ebd16e2864dcde7603f76
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-04-06 12:23:22 +0200

    Create id in localStorage for the user

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
-import CartStorage, { cart } from "@/components/cartstorage";
+import CartStorage from "@/components/cartstorage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export let ranOrNot = false;
 
 export default function App({ Component, pageProps }: AppProps) {
+	// If the user doesn't have an id, give them one
+	useEffect(() => {
+		if (!localStorage.getItem("id")) {
+			localStorage.setItem(
+				"id",
+				Math.random().toString(36).substring(2, 15) +
+					Math.random().toString(36).substring(2, 15)
+			);
+		}
+	}, []);
+
 	if (!ranOrNot) {
 		CartStorage();
 		ranOrNot = true;
