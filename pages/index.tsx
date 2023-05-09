d07e1defc093b623d67c649a494519bac2dee985
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-05-09 15:37:34 +0200

    Add Contentful locale to the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,66 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const { client } = require("../lib/contentful");
 
 export default function Home() {
+	const [languageStrings, setLanguageStrings] = useState<any>([
+		null,
+		null,
+		null,
+		null,
+		null,
+	]);
+
+	useEffect(() => {
+		// Check user locale
+
+		async function checkLanguage() {
+			const locale = window.navigator.language;
+			if (locale.startsWith("sv")) {
+				// Get entry 645bikC48FQqmGFdc9Iejv
+				await client
+					.getEntry("40tbz5JrFGf79QTZjsxvbF", {
+						locale: "sv",
+					})
+					.then((data: { fields: { frontPage: string } }) => {
+						setLanguageStrings(data?.fields?.frontPage);
+					});
+			} else {
+				await client
+					.getEntry("40tbz5JrFGf79QTZjsxvbF", {
+						locale: "en",
+					})
+					.then((data: { fields: { frontPage: string } }) => {
+						setLanguageStrings(data?.fields?.frontPage);
+					});
+			}
+		}
+		checkLanguage();
+	}, []);
+
 	return (
 		<>
 			<div className={styles.container}>
 				<Head>
-					<title>Johans vinshop</title>
+					<title>{languageStrings[0]}</title>
 					<meta name="description" content="Vinsida gjord av Johan Forsberg" />
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<link rel="icon" href="/favicon.ico" />
 				</Head>
 				<main>
 					<div className={styles.main}>
-						<div>Välkommen till min Vinshop!</div>
+						<div>{languageStrings[1]}</div>
 					</div>
 					<div>
 						<ul>
 							<li className={styles.wineList}>
-								<Link href={"/products?filter=rott"}>Rött vin</Link>
+								<Link href={"/products?filter=rott"}>{languageStrings[2]}</Link>
 							</li>
 							<li className={styles.wineList}>
-								<Link href={"/products?filter=vitt"}>Vitt vin</Link>
+								<Link href={"/products?filter=vitt"}>{languageStrings[3]}</Link>
 							</li>
 						</ul>
 					</div>
