9b1feb1e839a5559d5abbb402a6f4b257215b0f6
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-05-12 11:50:47 +0200

    Create a provisionary privacy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,13 @@
+export default function Privacy() {
+	return (
+		<div>
+			<h1>Privacy</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+				voluptatum, voluptate, quibusdam, quia voluptas quod dolorum voluptatem
+				quos quas quidem voluptatibus. Quisquam, quibusdam voluptas. Quisquam,
+				quibusdam voluptas. Quisquam, quibusdam
+			</p>
+		</div>
+	);
+}
