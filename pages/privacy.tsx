a4b747452cb0fa8f40afc3a8c282254f249d5889
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-05-15 11:11:48 +0200

    Make the CSS on Privacy look a bit nicer

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,12 +1,14 @@
 export default function Privacy() {
 	return (
-		<div className="m-8 flex border-spacing-1 justify-center rounded-sm border-2 border-double border-orange-400">
-			<p className="m-2 min-w-fit max-w-full">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-				voluptatum, voluptate, quibusdam, quia voluptas quod dolorum voluptatem
-				quos quas quidem voluptatibus. Quisquam, quibusdam voluptas. Quisquam,
-				quibusdam voluptas. Quisquam, quibusdam
-			</p>
+		<div className="m-8 flex justify-center">
+			<div className="flex justify-center rounded-sm border-2 border-double border-orange-400">
+				<p className="max-w-xl p-1">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+					voluptatum, voluptate, quibusdam, quia voluptas quod dolorum
+					voluptatem quos quas quidem voluptatibus. Quisquam, quibusdam
+					voluptas. Quisquam, quibusdam voluptas. Quisquam, quibusdam
+				</p>
+			</div>
 		</div>
 	);
 }
