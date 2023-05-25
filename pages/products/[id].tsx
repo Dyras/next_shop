6476a32272c64d68d04ebdbaf6ebae1658c8013b
Johan Forsberg <12563436+Dyras@users.noreply.github.com>
2023-05-25 14:26:00 +0200

    Print out the single product image

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,6 +2,7 @@ import { IProduct, IProductSaved } from "@/lib/iproduct";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
+import Image from "next/image";
 import { db } from "@/components/firebase";
 import { getAuth } from "firebase/auth";
 import { useCartCounter } from "@/lib/cartcounter";
@@ -56,6 +57,7 @@ export default function SingleProduct() {
 			}
 		};
 		getProduct();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [router.query]);
 
 	useEffect(() => {
@@ -124,34 +126,47 @@ export default function SingleProduct() {
 	if (!product) return null;
 
 	return (
-		<div>
-			<div>{product.name}</div>
-			{amount != 0 ? (
-				<div>
-					<div>
-						{amount} {contentfulStore?.productPage[2]}
-					</div>
-					<select
-						className="select"
-						value={amount}
-						onChange={(e) => setAmount(parseInt(e.target.value))}
-					>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-					</select>
-					<button onClick={removeFromCart}>
-						{contentfulStore?.productPage[1]}
-					</button>
+		<div className="flex justify-center">
+			<div className="dropshadow-md rouded-lg mt-12 w-80 bg-gray-700">
+				<div className="relative m-auto h-48 w-48 rounded border border-gray-400">
+					<Image
+						src={product?.imageUrl}
+						alt={product?.name}
+						fill
+						style={{ objectFit: "contain" }}
+						className="m-auto h-auto w-auto bg-gray-800"
+					/>
 				</div>
-			) : (
-				<button onClick={addtoCart}>{contentfulStore?.productPage[0]}</button>
-			)}
+				<div>{product.name}</div>
+				<div>{product.price} kr</div>
+				<div>{product.description}</div>
+				{amount != 0 ? (
+					<div>
+						<div>
+							{amount} {contentfulStore?.productPage[2]}
+						</div>
+						<select
+							className="select"
+							value={amount}
+							onChange={(e) => setAmount(parseInt(e.target.value))}
+						>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+						</select>
+						<button onClick={removeFromCart}>
+							{contentfulStore?.productPage[1]}
+						</button>
+					</div>
+				) : (
+					<button onClick={addtoCart}>{contentfulStore?.productPage[0]}</button>
+				)}
+			</div>
 		</div>
 	);
 }
